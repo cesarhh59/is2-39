2fa789f0e86a368838999269a17600098a683828
falucas <felix.arribas.lucas@gmail.com>
2018-10-23 22:50:16 +0200

    lista de usuarios global, registrar con todos los parametros.

--- backend/gestorUsuarios.js
+++ backend/gestorUsuarios.js
@@ -1,13 +1,20 @@
 var HashMap = require('hashmap');
+const util = require('util');
+
+var lista_usuarios = new HashMap();
+
+// Meto dos para hacer pruebas
+lista_usuarios.set('felix',{username:'felix', password:'felixpass', mail:'felix.arri@gmail.com', city:'Navalcarnero'});
+lista_usuarios.set('cesar',{username:'cesar', password:'cesarpass', mail:'cesar.herre@gmail.com', city:'Madrid'});
 
 /**
  * 
- * @param {String} username 
- * @param {String} password 
+ * @param {String} username
+ * @param {String} password
  * @return true si es correcto username y password, false e.o.c.
  */
 function login(username, password){
-    return getMapUsers().has(username) && (getMapUsers().search(password) == username);
+    return lista_usuarios.has(username) && (lista_usuarios.get(username).password == password);
 }
 
 /**
@@ -16,30 +23,32 @@ function login(username, password){
  * @param {String} password 
  * @return true si se completa correctamente (o ya existe -> login), false e.o.c.
  */
-function newUser(username, password){
-    if(getMapUsers().has(username)){//Ya existe
-        return (getMapUsers().search(password) == username);     
+function signup(username, password, mail, city){
+    if(lista_usuarios.has(username)){//Ya existe
+        console.log("El nombre de usuario " + username + " ya existe en el sistema.")
+        return (lista_usuarios.search(password) == username);
     }
     else{
-        let users = getMapUsers();
-        users.set(username,password);
+        var user =  {username:username, password:password, mail:mail, city:city};
+        lista_usuarios.set(username,user);
+        enviarLink(username,mail);
         return true;
     }
-
 }
 
-function getMapUsers(){
-    let hash = new HashMap(); 
-    hash.set('felix','felixpass');
-    hash.set('cesar','cesarpass');
-    return hash; // Para pruebas, debe recoger el hash map de todos los usuarios
+function enviarLink(username, mail){
+    var mensaje = "Te has dado de alta en la plataforma de compra-venta de comidas caseras, con el nombre de usuario: " 
+    + username + "\n\nPara confirmar que eres tú, por favor, haga click en el siguiente enlace:\n\n";
+    console.log("Se envia un correo a " + mail);
 }
 
 /*
 console.log(
     "\n\n login('felix','felixpass') -> " + login('felix','felixpass') + 
+    "\n\n lista_usuarios -> " + util.inspect(lista_usuarios) + 
     "\n\n login('noexiste','felixpass') -> " + login('noexiste','felixpass') + 
-    "\n\n newUser('pedro', 'pedropass') -> " + newUser('pedro', 'pedropass') + 
-    "\n\n newUser('cesar', 'cesarpass') -> " + newUser('cesar', 'cesarpass')
+    "\n\n signup('pedro', 'pedropass','pedro123@gmail.com','Madrid') -> " + signup('pedro', 'pedropass','pedro123@gmail.com','Madrid')  + 
+    "\n\n signup('cesar', 'cesarpass','cesitar@gmail.com','Barcelona') -> " + signup('cesar', 'cesarpass','cesitar@gmail.com','Barcelona') +
+    "\n\n lista_usuarios -> " + util.inspect(lista_usuarios)
 )
 */
