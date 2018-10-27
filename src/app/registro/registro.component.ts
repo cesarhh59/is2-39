dd41062ab2ac8697c0fa94dff83d8fed56b3b498
Cesar Herrero Herrero <cesarherrero252@gmail.com>
2018-10-27 19:13:37 +0200

    #registro > validaciones

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -9,9 +9,41 @@ export class RegistroComponent implements OnInit {
 
   public alergenos: string [] = [];
 
+  private registro;
+  private message: string [] = [];
   ngOnInit() {
     this.alergenos.push('Lactosa');
     this.alergenos.push('Celiaco');
   }
+  validate(): void {
+    if (this.registro) {
+      if (this.registro.nombre) {
+        this.message.push('El campo nombre no puede estar vacío');
+      }
+      if (this.registro.contraseña) {
+        this.message.push('El campo contraseña no puede estar vacío');
+      }
+      if (this.registro.contacto) {
+        this.message.push('El campo contacto no puede estar vacío');
+      }
+      if (this.registro.ciudad) {
+        this.message.push('El campo ciudad no puede estar vacío');
+      }
+      if (this.registro.contacto) {
+        this.message.push('El campo contacto no puede estar vacío');
+      }
+      if (this.message.length === 0) {
+        this.message.push('El registro se ha realizado correctamente, por favor vaya a su correo para validar el usuario');
+      }
+}
+
+// tslint:disable-next-line:no-empty-interface
+export interface ReigstroModel {
+  nombre: string;
+  contraseña: string;
+  email: string;
+  ciudad: string;
+  contacto: number;
+  alergenos: string[];
 
 }
