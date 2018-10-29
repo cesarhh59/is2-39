6189806388b368494416d1be3f2974ec407232ec
Cesar Herrero Herrero <cesarherrero252@gmail.com>
2018-10-29 19:21:35 +0100

    #chats > posicion mensajes

--- src/app/chats/chats.component.ts
+++ src/app/chats/chats.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chats.component.css']
 })
 export class ChatsComponent implements OnInit {
-
-  constructor() { }
+public chats: String [] = [];
+  constructor() {
+  }
 
   ngOnInit() {
+    this.chats.push('Raul');
+    this.chats.push('Pablo');
+    this.chats.push('Romina');
+    this.chats.push('Justo');
   }
 
 }
