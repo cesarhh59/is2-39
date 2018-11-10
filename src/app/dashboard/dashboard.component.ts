67f28868bdeba013372bcc79990d4d9b517ba04b
Cesar Herrero Herrero <cesarherrero252@gmail.com>
2018-11-10 18:28:12 +0100

    #dashboard > listarAnuncios

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IAnuncio } from '../gestor/gestor.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  public anuncios: IAnuncio [] = [];
   constructor() { }
 
   ngOnInit() {
+    const anuncio = {
+      titulo: 'Magdalenas',
+      porciones: 4,
+      localizacion: 'Madrid',
+      disponibles: true,
+      propietario: 'Jose',
+      estado: true,
+      valoracion: 4.1
+    };
+    this.anuncios.push(anuncio);
+    this.anuncios.push(anuncio);
+    // TODO llamada a servicios que devuelve los anuncios
   }
 
 }
