d14d152e94d321127ae5abed2475f5971187562b
Cesar Herrero Herrero <cesarherrero252@gmail.com>
2018-12-02 18:16:25 +0100

    #chats > logica cerrar chats y navegacion chats

--- src/app/chats/chats.component.ts
+++ src/app/chats/chats.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-chats',
@@ -9,29 +10,41 @@ export class ChatsComponent implements OnInit {
 public chats: String [] = [];
 public messages: IMessages [] = [];
 public msgSend: String = '';
-constructor() {
+constructor( private _router: ActivatedRoute) {
   }
 
   ngOnInit() {
+
+  // TODO recuperar todos los chats de un usuario
+
+  // Recuperamos el chat activo
+    const usuario: string = this._router.snapshot.paramMap.get('id');
+    console.log(usuario);
+
+    if (usuario && usuario !== '') {
+    // TODO cargar chats de ese usuario
+    this.messages.push({
+      user: 'me',
+       msg: 'Tienes algo para mi'
+     });
+     this.messages.push({
+       user: 'Julio',
+        msg: 'La coca'
+      }); this.messages.push({
+       user: 'me',
+        msg: 'For me'
+      }); this.messages.push({
+       user: 'me',
+        msg: 'yas'
+      });
+
+    }
     this.chats.push('Raul');
     this.chats.push('Pablo');
     this.chats.push('Romina');
     this.chats.push('Justo');
 
-    this.messages.push({
-     user: 'me',
-      msg: 'Tienes algo para mi'
-    });
-    this.messages.push({
-      user: 'Julio',
-       msg: 'La coca'
-     }); this.messages.push({
-      user: 'me',
-       msg: 'For me'
-     }); this.messages.push({
-      user: 'me',
-       msg: 'yas'
-     });
+
 
   }
   send(): void {
