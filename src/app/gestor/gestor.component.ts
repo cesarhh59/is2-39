adb1f8effa585b985047c774031983ced5e12b1a
Cesar Herrero Herrero <cesarherrero252@gmail.com>
2018-12-04 17:22:50 +0100

    #gestor > validaciones

--- src/app/gestor/gestor.component.ts
+++ src/app/gestor/gestor.component.ts
@@ -11,7 +11,8 @@ export class GestorComponent implements OnInit {
   public options: string[] = [];
   public activo: Boolean = false;
   constructor(private platosService: AnunciosService) { }
-
+  public message: string [] = [];
+  public typeMessage = 'danger';
   ngOnInit() {
     this.platosService.getPlatosPropietario(localStorage.getItem('token')).subscribe((res: IResponse) => {
     this.options.push(res.platos);
@@ -22,20 +23,43 @@ saveAlergeno(alergeno: any) {
 
   }
   saveAnuncio(titulo: string, porciones: number, localizacion: string) {
-    const anuncio: IAnuncio = {
-      titulo: titulo,
-      porciones: porciones,
-      localizacion: localizacion,
-      disponibles: true,
-      propietario: localStorage.getItem('token'),
-      estado: false,
-      valoracion: 0
-    };
-    this.platosService.addPlato(anuncio).subscribe((res: IResponse) => {
-      console.log(res);
-    });
+    this.message = [];
+    if ( this.checkCampos(titulo, porciones, localizacion)) {
+      const anuncio: IAnuncio = {
+        titulo: titulo,
+        porciones: porciones,
+        localizacion: localizacion,
+        disponibles: true,
+        propietario: localStorage.getItem('token'),
+        estado: false,
+        valoracion: 0
+      };
+      this.platosService.addPlato(anuncio).subscribe((res: IResponse) => {
+      });
+      this.message.push('El plato se ha creado correctamente');
+      this.typeMessage = 'success';
+    }
+  }
+  checkCampos(titulo: string, porciones: number, localizacion: string): boolean {
+    let result = true;
+    if (titulo === '') {
+      result = false;
+      this.message.push('Indique el titulo del plato');
+    }
+    if (porciones.toString() === '' || porciones > 0) {
+      result = false;
+      this.message.push('El número de porciones no puede ser 0');
+    }
+    if ( localizacion === '') {
+      result = false;
+      this.message.push('Indique la localización del plato');
+    }
+
+    return result;
+
   }
 }
+
 
 export interface IAnuncio {
   titulo: String;
